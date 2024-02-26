12c828a3670565f0f8eb86940cd56cdb8b0ed66e
pythonicoder <sinankasikci6@gmail.com>
2024-02-26 18:48:05 +0300

    Added a new project

---
+++ Day5/Password_generator.py
@@ -0,0 +1,60 @@
+# Password generator
+
+import random as rn
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input('How many letters would you like in your password?\n'))
+nr_numbers = int(input('How many numbers would you like in your password?\n'))
+nr_symbols = int(input('How many symbols would you like in your password?\n'))
+
+######*****************************######
+# Eazy Level                            |
+#                                       |
+# password = ""                         |
+#                                       |
+# for char in range(1, nr_letters + 1): |
+#   password += rn.choice(letters)      |
+#                                       |
+# for char in range(1, nr_symbols + 1): |
+#   password += rn.choice(symbols)      |
+#                                       |
+# for char in range(1, nr_numbers + 1): |
+#   password += rn.choice(numbers)      |
+#                                       |
+# print(password)                       |
+#                                       |
+######*****************************######
+
+# Hard Level
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list += rn.choice(letters)
+
+for char in range(1, nr_symbols + 1):
+    password_list += rn.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+    password_list += rn.choice(numbers)
+
+rn.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+    
+print(password)
+    
+
+
+
+
+
+
+
+    
